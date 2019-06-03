5581722972b8b72e35c2d641f438a109a5271c11
Greg-tts <gtesto@techtalentsouth.com>
2019-06-03 15:07:05 -0700

    saved current changes

--- src/main/java/com/dojo/miyagi/blogPost/BlogPost.java
+++ src/main/java/com/dojo/miyagi/blogPost/BlogPost.java
@@ -1,5 +1,53 @@
 package com.dojo.miyagi.blogPost;
 
-public class BlogPost {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class BlogPost {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long postId;
+	private String name;
+	private String author;
+	private String body;
+	
+	public BlogPost() {};
+	
+	public BlogPost(String name, String author, String body) {
+		this.name = name;
+		this.author = author;
+		this.body = body;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	@Override
+	public String toString() {
+		return "BlogPost [postId=" + postId + ", name=" + name + ", author=" + author + ", body=" + body + "]";
+	}
 }
