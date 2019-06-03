67d85a04c92eabcb98de1fb938b96f92e5e5ad99
Greg-tts <gtesto@techtalentsouth.com>
2019-06-03 16:42:43 -0700

    Connected everything

--- src/main/java/com/dojo/miyagi/blogPost/BlogPostController.java
+++ src/main/java/com/dojo/miyagi/blogPost/BlogPostController.java
@@ -18,6 +18,14 @@ public class BlogPostController {
 	public String index(BlogPost blogPost) {
 		return "blogpost/index";
 	}
+	
 	@PostMapping(value="/blogpost")
+	public String addNewBlogPost(BlogPost blogPost, Model model) {
+//		blogPostRepository.save(new BlogPost(blogPost.getName(), blogPost.getAuthor(), blogPost.getBody()));
+		model.addAttribute("name", blogPost.getName());
+		model.addAttribute("author", blogPost.getAuthor());
+		model.addAttribute("body", blogPost.getBody());
+		return "blogpost/result";
+	}
 
 }
